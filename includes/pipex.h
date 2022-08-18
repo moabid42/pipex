1b186b7cb7bc68e384eb660dcc7260f76216d1f3
moabid <moabid@student.42heilbronn.de>
2022-08-18 03:55:06 +0200

    Fixed some issues

---
+++ includes/pipex.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moabid <moabid@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/11 06:29:17 by moabid            #+#    #+#             */
+/*   Updated: 2022/05/26 17:11:32 by moabid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <stdlib.h>
+
+# define FALSE	0
+# define TRUE	1
+
+void	freeme(char **paths);
+int		openfile(char *file, int re_or_wr);
+
+void	executeme(char *cmd, char **env);
+void	childprocess(char *cmd, char **env);
+void	execute_heredoc(char **argv, int *pfd);
+void	handle_heredoc(int ac, char **av);
+
+char	*parser(char *cmd, char *paths);
+char	*get_path(char *cmd, char **env);
+
+void	error_file(char *file);
+void	error(int n_exit);
+void	error_command(char *cmd);
+
+
+#endif
---
+++ src/pipex_bonus.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moabid <moabid@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 17:45:55 by moabid            #+#    #+#             */
+/*   Updated: 2022/05/26 17:19:41 by moabid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex_bonus.h"
+
+void	executeme(char *cmd, char **env)
+{
+	char	**s_cmd;
+	char	*path;
+
+	s_cmd = ft_split(cmd, ' ');
+	path = get_path(s_cmd[0], env);
+	if (path == FALSE)
+		exit(EXIT_FAILURE);
+	if (execve(path, s_cmd, env) == -1)
+	{
+		error_command(s_cmd[0]);
+		freeme(s_cmd);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	childprocess(char *cmd, char **env)
+{
+	pid_t	pid;
+	int		pfd[2];
+
+	if (pipe(pfd) == -1)
+		error(1);
+	pid = fork();
+	if (pid == -1)
+		error(1);
+	if (!pid)
+	{
+		close(pfd[0]);
+		dup2(pfd[1], 1);
+		executeme(cmd, env);
+	}
+	else
+	{
+		close(pfd[1]);
+		dup2(pfd[0], 0);
+		waitpid(pid, NULL, 0);
+	}
+}
+
+void	execute_heredoc(char **argv, int *pfd)
+{
+	char	*line;
+	close(pfd[0]);
+	while (1)
+	{
+		write(1, "\033[31mEvilPipex 👻> ", 23);
+		write(1, "\033[0m", 5);
+		line = get_next_line(0);
+		if (ft_strncmp(line, argv[2], ft_strlen(argv[2])) == 0)
+		{
+			free(line);
+			exit(0);
+		}
+		ft_putstr_fd(line, pfd[1]);
+		free(line);
+	}
+}
+
+void	handle_heredoc(int ac, char **av)
+{
+	pid_t	pid;
+	int		pfd[2];
+
+	if (ac < 6)
+		error(1);
+	if (pipe(pfd) == -1)
+		error(1);
+	pid = fork();
+	if (pid == -1)
+		error(1);
+	if (!pid)
+		execute_heredoc(av, pfd);
+	else
+	{
+		close(pfd[1]);
+		dup2(pfd[0], 0);
+		wait(NULL);
+	}
+}
+
+int	main(int ac, char **av, char **env)
+{
+	int		i;
+	int		fd_in;
+	int		fd_out;
+
+	if (ac < 5)
+		error(1);
+	if (ft_strncmp(av[1], "here_doc", 8) == 0)
+	{
+		i = 3;
+		fd_out = openfile(av[ac - 1], 2);
+		handle_heredoc(ac, av);
+	}
+	else
+	{
+		i = 2;
+		fd_in = openfile(av[1], 0);
+		fd_out = openfile(av[ac - 1], 1);
+		dup2(fd_in, 0);
+	}
+	while (i < ac - 2)
+		childprocess(av[i++], env);
+	dup2(fd_out, 1);
+	executeme(av[ac - 2], env);
+}
